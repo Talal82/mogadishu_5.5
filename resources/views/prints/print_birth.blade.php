68037666e2d7b2a84aed098093f5451828d496ca
Talal82 <talalanwer2424@gmail.com>
2018-10-15 16:38:56 +0500

    modify print pages

--- resources/views/prints/print_birth.blade.php
+++ resources/views/prints/print_birth.blade.php
@@ -3,123 +3,193 @@
 <head>
 	<title>{{ config('app.name') }} - Birth Certificate</title>
 	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-
-	<link rel="stylesheet" type="text/css" href="">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+    
+    
 	<style>
-	.container{
+	body {
+		font-family: Times New Roman;
+	}
+	input {
+	    border: 2px solid;
+	    background: white;
+	    font-size: 20px;
+	}
+	.form-control{
+	    border: 2px solid;
+	    font-size: 21px;
+	    font-family: Bodoni MT;
+	    height: 50px;
+	}
+	.p-l-15{
+	    padding-left: 15px;
+	}
+	.p-r-15{
+	    padding-right: 15px;
+	}
+	.container1{
 		margin-top: 100px;
 		margin-bottom: 50px;
+		margin-left: 2cm;
+		margin-right: 1.75cm;
 		font-size: 20px;
 	}
+	.fs-15{
+	    font-size: 20px;
+	}
+	.m-b-0{
+	    margin-bottom: 0px;
+	}
+	.m-t-10{
+	    margin-top: 10px;
+	}
 	.m-t-20{
-		margin-top: 20px;
+		margin-top: 17px;
 	}
 	.p-t-5{
 		padding-top: 5px;
+	}
+	.bold{
+	    font-style: bold;
+	}
+	.italic{
+	    font-style: italic;
+	}
+	.font{
+	    font-family: Bodoni MT;
+        font-size: 19px;
+        font-weight: bold;
+	}
+	.font1{
+	    font-family: Bodoni MT;
+        font-size: 18px;
+        font-weight: bold;
 	}
 
 	@media print
 	{
 		* {-webkit-print-color-adjust:exact;}
-		.container{
-			margin-top: 300px;
+		.container1{
+			margin-top: 190px;
+			margin-left: 2cm;
+			margin-right: 1.75cm;
 		}
 		.display-none{
 			display: none;
+		}
+		.bar-container{
+		    margin-left:20px;
+		    margin-top: 20px;
 		}
 	}
 </style>
 
 </head>
 <body>
-	<div class="container">
-		<h1 class="text-center">WARQADDA DHALASHADA</h1>
-		<h4 class="text-center" style="margin-top: 2px;">Birth Certificate</h4><br>
+    <div class="row bar-container" style="margin-left: 40px; margin-top:40px;">
+        <div class="col-md-3 text-center">
+            <h5><b>Compurter Serial#</b></h5>
+            <h5><b>{{ $report -> serial_number }}</b></h5>
+            <img id="code39" style="height: 40px;"></img>
+        </div>
+    </div>
+	<div class="container1">
+		<h1 class="text-center" style="font-family: Bodoni MT Condensed; margin-bottom: 0px;"><b>WARQADDA DHALASHADA</b></h1>
+		<h4 class="text-center" style="font-family: Bodoni MT Condensed; margin-bottom: 30px;"><b>Birth Certificate</b></h4>
 		<div class="row">
-			<div class="col-md-10 offset-1">
+			<div class="col-md-12">
 				<div class="row">
-					<div class="col-md-3 p-t-5">
-						<strong>Full Name</strong>
+					<div class="col-md-4">	
+						<strong class="fs-15 font">MAGACA</strong><br>
+						<p class="m-b-0 fs-15 font1">Full Name</p>
 					</div>
-					<div class="col-md-9">
-						<input class="form-control text-center" type="text" readonly value="{{ strtoupper($report -> full_name) }}">
-					</div>
-				</div>
-
-				<div class="row m-t-20">
-					<div class="col-md-3 p-t-5">
-						<strong>Date of Birth</strong>
-					</div>
-					<div class="col-md-9">
-						<input class="form-control" type="text" readonly value="{{ date('j-M-Y' , strtotime($report -> birth_date)) }}">
+					<div class="col-md-8" style="margin">
+						<input  class="form-control m-t-10 text-center" type="text" readonly style="font-weight: bold; background: #fff; font-style: bold; font-size: 22px; font-family: Bodoni MT Black; color: #000" value="{{ strtoupper($report -> full_name) }}">
 					</div>
 				</div>
 
 				<div class="row m-t-20">
-					<div class="col-md-3 p-t-5">
-						<strong>Place of Birth</strong>
+					<div class="col-md-4 p-t-5">
+						<strong class="fs-15 font">TAARIKHDA DHALASHADA</strong><br>
+						<p class="m-b-0 fs-15 font1">Date of Birth</p>
 					</div>
-					<div class="col-md-9">
-						<input class="form-control" type="text" readonly value="{{ strtoupper($report -> birth_place) }}">
-					</div>
-				</div>
-
-				<div class="row m-t-20">
-					<div class="col-md-3 p-t-5">
-						<strong>Gender</strong>
-					</div>
-					<div class="col-md-9">
-						<input class="form-control" type="text" readonly value="{{ $report -> gender }}">
+					<div class="col-md-8">
+						<input class="form-control m-t-10" type="text" readonly
+						style="background: #fff; color: #000;" value="{{ date('j-M-Y' , strtotime($report -> birth_date)) }}">
 					</div>
 				</div>
 
 				<div class="row m-t-20">
-					<div class="col-md-3 p-t-5">
-						<strong>Father's Name</strong>
+					<div class="col-md-4 p-t-5">
+						<strong class="fs-15 font">GOOBTA DHALASHADA</strong><br>
+						<p class="m-b-0 fs-15 font1">Place of Birth</p>
 					</div>
-					<div class="col-md-9">
-						<input class="form-control" type="text" readonly value="{{ strtoupper($report -> father_name) }}">
-					</div>
-				</div>
-
-				<div class="row m-t-20">
-					<div class="col-md-3 p-t-5">
-						<strong>Mother's Name</strong>
-					</div>
-					<div class="col-md-9">
-						<input class="form-control" type="text" readonly value="{{ strtoupper($report -> mother_name) }}">
-					</div>
-				</div>
-				<div class="row m-t-20">
-					<div class="col-md-3 p-t-5">
-						<strong>Address</strong>
-					</div>
-					<div class="col-md-9">
-						<input class="form-control" type="text" readonly value="{{ $report -> address }}">
+					<div class="col-md-8">
+						<input class="form-control m-t-10" type="text" readonly value="{{ strtoupper($report -> birth_place) }}" style="background: #fff; color: #000;">
 					</div>
 				</div>
 
 				<div class="row m-t-20">
-					<div class="col-md-3 p-t-5">
-						<strong>Date of Issue</strong>
+					<div class="col-md-4 p-t-5">
+						<strong class="fs-15 font">JINSI</strong><br>
+						<p class="m-b-0 fs-15 font1">Gender</p>
 					</div>
-					<div class="col-md-9">
-						<input class="form-control" type="text" readonly value="{{ date('j-M-Y' , strtotime($report -> issue_date)) }}">
+					<div class="col-md-8">
+						<input class="form-control m-t-10" type="text" readonly value="{{ $report -> gender }}" style="background: #fff; color: #000;">
 					</div>
 				</div>
 
-				<div class="row" style="margin-top: 80px;">
+				<div class="row m-t-20">
+					<div class="col-md-4 p-t-5">
+						<strong class="fs-15 font">MAGACA AABAHA</strong><br>
+						<p class="m-b-0 fs-15 font1">Father Name</p>
+					</div>
+					<div class="col-md-8">
+						<input class="form-control m-t-10" type="text" readonly value="{{ strtoupper($report -> father_name) }}" style="background: #fff; color: #000;">
+					</div>
+				</div>
+
+				<div class="row m-t-20">
+					<div class="col-md-4 p-t-5">
+						<strong class="fs-15 font">MAGACA HOOYADA</strong><br>
+						<p class="m-b-0 fs-15 font1">Mother Name</p>
+					</div>
+					<div class="col-md-8">
+						<input class="form-control m-t-10" type="text" readonly value="{{ strtoupper($report -> mother_name) }}" style="background: #fff; color: #000;">
+					</div>
+				</div>
+				<div class="row m-t-20">
+					<div class="col-md-4 p-t-5">
+						<strong class="fs-15 font">DEGGAN</strong><br>
+						<p class="m-b-0 fs-15 font1">Address</p>
+					</div>
+					<div class="col-md-8">
+						<input class="form-control m-t-10" type="text" readonly value="{{ $report -> address }}" style="background: #fff; color: #000;">
+					</div>
+				</div>
+
+				<div class="row m-t-20">
+					<div class="col-md-4 p-t-5">
+						<strong class="fs-15 font">TAARIIKHDA LA BIXIYEY</strong><br>
+						<p class="m-b-0 fs-15 font1">Date of Issue</p>
+					</div>
+					<div class="col-md-8">
+						<input class="form-control m-t-10" type="text" readonly value="{{ date('j-M-Y' , strtotime($report -> issue_date)) }}" style="background: #fff; color: #000;">
+					</div>
+				</div>
+
+				<div class="row" style="margin-top: 70px;">
 					<div class="col-md-12">
 						<div class="row">
 							<div class="col-md-3">
-								<img src="{{ asset('images/stamp.png') }}" height="200" width="200">
+								<img src="{{ asset('public/images/stamp.png') }}" height="187" width="200">
 							</div>
 							<div class="col-md-9">
 								<div class="row">
-									<img src="{{ asset('images/duqa.jpg') }}" height="100" width="400">
+									<img src="{{ asset('public/images/duqa.JPG') }}" height="100" width="400">
 								</div>
 								<div class="row">
-									<img src="{{ asset('images/signature.png') }}" height="100" width="400">
+									<img src="{{ asset('public/images/signature.png') }}" height="100" width="400">
 								</div>
 							</div>
 						</div>
@@ -128,9 +198,9 @@
 
 
 
-				<a href="#" class="display-none btn btn-success float-left m-t-20" id="print">print</a>
+				<a href="#" class="display-none btn btn-success float-left m-t-20" id="print"><i class="fa fa-print"></i> Print</a>
 
-				<a href="{{ URL::previous() }}" class="display-none btn btn-primary float-right m-t-20">back</a>
+				<a href="{{ URL::previous() }}" class="display-none btn btn-primary float-right m-t-20"><i class="fa fa-arrow-left"></i> Back</a>
 			</div>
 			
 		</div>
@@ -140,7 +210,7 @@
 	
 	
 
-
+    <script src="https://cdn.jsdelivr.net/jsbarcode/3.3.18/JsBarcode.all.min.js"></script>
 	<script
 	src="https://code.jquery.com/jquery-3.3.1.min.js"
 	integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
@@ -149,6 +219,13 @@
 		$('#print').on('click', function(e){
 			e.preventDefault();
 			window.print();
+		});
+		JsBarcode("#code39", "{{ $report -> serial_number }}",{
+			format: "pharmacode",
+            lineColor: "#000",
+            width: 7,
+            height: 40,
+            displayValue: false
 		});
 	</script>
 </body>
--- resources/views/prints/print_id.blade.php
+++ resources/views/prints/print_id.blade.php
@@ -1,15 +1,36 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>{{ config('app.name') }} - Birth Certificate</title>
+	<title>{{ config('app.name') }} - ID Certificate</title>
 	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
 
-	<link rel="stylesheet" type="text/css" href="">
 	<style>
-	.container{
+	body {
+		font-family: Times New Roman;
+	}
+	
+	.container1{
 		margin-top: 100px;
 		margin-bottom: 50px;
+		margin-left: 1.4cm;
+		margin-right: 1.4cm;
 		font-size: 20px;
+	}
+	.fs-15{
+	    font-size: 18px;
+	}
+	.m-b-0{
+	    margin-bottom: 0px;
+	}
+	.m-t-10{
+	    margin-top: 10px;
+	}
+	.m-t-0{
+	    margin-top: 0px;
+	}
+	.p-t-0{
+	    padding-top: 0px;
 	}
 	.m-t-20{
 		margin-top: 20px;
@@ -17,38 +38,58 @@
 	.p-t-5{
 		padding-top: 5px;
 	}
+	p{
+	    margin-bottom: 0px;
+	    font-size: 20px;
+	}
+	.l-h{
+	    line-height:8px;
+	    padding-bottom: 8px;
+	}
 
 	@media print
 	{
 		* {-webkit-print-color-adjust:exact;}
-		.container{
-			margin-top: 400px;
+		.container1{
+			margin-top: 190px;
+			margin-left: 1.4cm;
+			margin-right: 1.4cm;
 		}
 		.display-none{
 			display: none;
+		}
+		.bar-container{
+		    margin-left:40px;
+		    margin-top: 40px;
 		}
 	}
 </style>
 
 </head>
 <body>
-	<div class="container">
-		<h1 class="text-center">WARQADDA DHALASHADA</h1>
-		<h4 class="text-center" style="margin-top: 2px; border-bottom: 2px solid black; padding-bottom: 10px;"><u>Certificate Of Identity</u></h4>
-		{{-- <div class="row">
-			<div class="text-center">
-				<strong>
-					DUQA MAGAALDA MUQDISHO WUXUU CADDEYNAYAA IN SAWIRKA IYO SUUL SAARIDDA HOOS KU TILMAAMAN UU YAHAY QUFKA HOOS KU XUSAN:
-				</strong>
-			</div>
-		</div> --}}
-		<div class="row" style="margin-bottom: 30px;">
-			<div class="text-center">
-				<strong>
-					<i>
-						The Mayor of Mogadishu hereby certifies that the person whose picture and thumb print appears here has the following details:
-					</i>
-				</strong>
+    <div class="row bar-container" style="margin-left: 40px; margin-top:40px;">
+        <div class="col-md-3 text-center">
+            <h5><b>Compurter Serial#</b></h5>
+            <h5><b>{{ $report -> serial_number }}</b></h5>
+            <img id="code39" style="height: 40px;"></img>
+        </div>
+    </div>
+	<div class="container1">
+		<h1 class="text-center" style="font-family: Bodoni MT Condensed; margin-bottom:0px;"><b>WARQADDA SUGNAANTA</b></h1>
+		<h4 class="text-center" style="font-size:17px; border-bottom: 2px solid black; padding-bottom: 10px; font-family:Bodoni MT Condensed;"><b><u>Certificate of Identity confirmation</u></b></h4>
+		<div class="row">
+			    <div class="col-md-12">
+			        <strong>
+					    DUQA MAGAALDA MUQDISHO WUXUU CADDEYNAYAA IN SAWIRKA IYO SUUL SAARIDDA<br>
+				        HOOS KU TILMAAMAN UU YAHAY QUFKA HOOS KU XUSAN:
+				    </strong>
+			    </div>
+		</div>
+		<div class="row" style="margin-bottom: 10px;">
+			<div class="col-md-12" style="text-align: justify;">
+					<p style="font-size: 17px; text-align: justify;">
+						<b><i>(The Mayor of Mogadishu hereby certifies that the person whose picture and thumb print appears here has the following details:)</i></b>
+					</p>
 			</div>
 		</div>
 		<div class="row">
@@ -56,114 +97,125 @@
 				<div class="row">
 					<div class="col-md-12 col-lg-12">
 						<div class="row">
-							<div class="col-md-8 col-lg-8">
+							<div class="col-md-9 col-lg-9">
 								<div class="row">
-									<div class="col-md-4 p-t-5">
-										<strong>Full Name</strong>
+									<div class="col-md-6 m-b-0">
+										<strong class="fs-15 m-t-0 m-b-0">MAGACA</strong><br>
+						                <p class="m-b-0 fs-15 m-t-0 l-h">Full Name</p>
 									</div>
-									<div class="col-md-8">
-										<strong><u>{{ strtoupper($report -> full_name) }}</u></strong>
+									<div class="col-md-6">
+										<strong style="font-family: Bodoni MT; font-size: 20px;"><b>{{ strtoupper($report -> full_name) }}</b></strong>
 									</div>
 								</div>
-								<div class="row p-t-5">
-									<div class="col-md-4">
-										<strong>Date Of Birth</strong>
+								<div class="row">
+									<div class="col-md-6">
+										<strong class="fs-15">TAARIIKHDA DHALASHADA</strong><br>
+						                <p class="m-b-0 fs-15 l-h">Date of Birth</p>
 									</div>
-									<div class="col-md-8">
+									<div class="col-md-6">
 										<p>{{ date('j-M-Y' , strtotime($report -> birth_date)) }}</p>
 									</div>
 								</div>
-								<div class="row p-t-5">
-									<div class="col-md-4">
-										<strong>Place Of Birth</strong>
+								<div class="row">
+									<div class="col-md-6">
+										<strong class="fs-15">GOOBTA DHALASHADA</strong><br>
+						                <p class="m-b-0 fs-15 l-h">Place of Birth</p>
 									</div>
-									<div class="col-md-8">
+									<div class="col-md-6">
 										<p>{{ strtoupper($report -> birth_place) }}</p>
 									</div>
 								</div>
-								<div class="row p-t-5">
-									<div class="col-md-4">
-										<strong>ID Number</strong>
+								<div class="row">
+									<div class="col-md-6">
+										<strong class="fs-15">NUMBERKA KAARKA AQOONSIGA</strong><br>
+						                <p class="m-b-0 fs-15 l-h">ID Number</p>
 									</div>
-									<div class="col-md-8">
+									<div class="col-md-6">
 										<p>{{ strtoupper($report -> serial_number) }}</p>
 									</div>
 								</div>
-								<div class="row p-t-5">
-									<div class="col-md-4">
-										<strong>Gender</strong>
+								<div class="row">
+									<div class="col-md-6">
+										<strong class="fs-15">JINSI</strong><br>
+						                <p class="m-b-0 fs-15 l-h">Gender</p>
 									</div>
-									<div class="col-md-8">
+									<div class="col-md-6">
 										<p>{{ $report -> gender }}</p>
 									</div>
 								</div>
-								<div class="row p-t-5">
-									<div class="col-md-4">
-										<strong>Marital Status</strong>
+								<div class="row">
+									<div class="col-md-6">
+										<strong class="fs-15">XAALADA GUURKA</strong><br>
+						                <p class="m-b-0 fs-15 l-h">Marital Status</p>
 									</div>
-									<div class="col-md-8">
+									<div class="col-md-6">
 										<p>{{ $report -> marital_status }}</p>
 									</div>
 								</div>
-								<div class="row p-t-5">
-									<div class="col-md-4">
-										<strong>Address</strong>
+								<div class="row">
+									<div class="col-md-6">
+										<strong class="fs-15">DEGGAN</strong><br>
+						                <p class="m-b-0 fs-15 l-h">Address</p>
 									</div>
-									<div class="col-md-8">
+									<div class="col-md-6">
 										<p>{{ $report -> address }}</p>
 									</div>
 								</div>
-								<div class="row p-t-5">
-									<div class="col-md-4">
-										<strong>Mother's Name</strong>
+								<div class="row">
+									<div class="col-md-6">
+										<strong class="fs-15">MAGACA HOOYADA</strong><br>
+						                <p class="m-b-0 fs-15 l-h">Mother Name</p>
 									</div>
-									<div class="col-md-8">
+									<div class="col-md-6">
 										<p>{{ strtoupper($report -> mother_name) }}</p>
 									</div>
 								</div>
-								<div class="row p-t-5">
-									<div class="col-md-4">
-										<strong>Date Of Issue</strong>
+								<div class="row">
+									<div class="col-md-6">
+										<strong class="fs-15">TARIIKHDA LA BIXIYEY</strong><br>
+						                <p class="m-b-0 fs-15 l-h">Date of Issue</p>
 									</div>
-									<div class="col-md-8">
+									<div class="col-md-6">
 										<p>{{ date('j-M-Y' , strtotime($report -> issue_date)) }}</p>
 									</div>
 								</div>
-								<div class="row p-t-5">
-									<div class="col-md-4">
-										<strong>Occupation</strong>
+								<div class="row">
+									<div class="col-md-6">
+										<strong class="fs-15">SHAQADA</strong><br>
+						                <p class="m-b-0 fs-15 l-h">Occupation</p>
 									</div>
-									<div class="col-md-8">
+									<div class="col-md-6">
 										<p>{{ ucwords($report -> occupation) }}</p>
 									</div>
 								</div>
 							</div>
-							<div class="col-md-4 col-lg-4 m-t-20">
-								<div class="row">
+							<div class="col-md-3 col-lg-3 m-t-20">
+								<div class="row" style="margin-top: 50px;">
 									<div class="col-md-12 col-lg-12">
-										<img src="{{ asset('uploads/reports_images/'.$report -> main_image) }}" height="200" width="200">
+										<img src="{{ asset('public/uploads/reports_images/'.$report -> main_image) }}" height="200" width="200" style="border: 2px solid;">
 									</div>
 								</div>
 								<div class="row m-t-20">
 									<div class="col-md-12 col-lg-12">
-										<img src="{{ asset('uploads/reports_images/'.$report -> thumb_image) }}" height="200" width="200">
+										<img src="{{ asset('public/uploads/reports_images/'.$report -> thumb_image) }}" height="200" width="200" style="border: 2px solid;">
+										<p style="margin-left: 30px; font-family: Times New Roman; font-size:18px"><i>Right Thumb Print</i></p>
 									</div>
 								</div>
 							</div>
 						</div>
 
-						<div class="row" style="margin-top: 80px;">
+						<div class="row" style="margin-top: 120px;">
 							<div class="col-md-12">
 								<div class="row">
 									<div class="col-md-3">
-										<img src="{{ asset('images/stamp.png') }}" height="200" width="200">
+										<img src="{{ asset('public/images/stamp.png') }}" height="187" width="200">
 									</div>
 									<div class="col-md-9">
 										<div class="row">
-											<img src="{{ asset('images/duqa.jpg') }}" height="100" width="400">
+											<img src="{{ asset('public/images/duqa.JPG') }}" height="100" width="400">
 										</div>
 										<div class="row">
-											<img src="{{ asset('images/signature.png') }}" height="100" width="400">
+											<img src="{{ asset('public/images/signature.png') }}" height="100" width="400">
 										</div>
 									</div>
 								</div>
@@ -174,9 +226,9 @@
 				</div>
 
 
-				<a href="#" class="display-none btn btn-success float-left m-t-20" id="print">print</a>
+				<a href="#" class="display-none btn btn-success float-left m-t-20" id="print"><i class="fa fa-print"></i> Print</a>
 
-				<a href="{{ URL::previous() }}" class="display-none btn btn-primary float-right m-t-20">back</a>
+				<a href="{{ URL::previous() }}" class="display-none btn btn-primary float-right m-t-20"><i class="fa fa-arrow-left"></i> Back</a>
 			</div>
 			
 		</div>
@@ -186,7 +238,7 @@
 	
 	
 
-
+    <script src="https://cdn.jsdelivr.net/jsbarcode/3.3.18/JsBarcode.all.min.js"></script>
 	<script
 	src="https://code.jquery.com/jquery-3.3.1.min.js"
 	integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
@@ -195,6 +247,13 @@
 		$('#print').on('click', function(e){
 			e.preventDefault();
 			window.print();
+		});
+		JsBarcode("#code39", "{{ $report -> serial_number }}",{
+			format: "pharmacode",
+            lineColor: "#000",
+            width: 7,
+            height: 40,
+            displayValue: false
 		});
 	</script>
 </body>
